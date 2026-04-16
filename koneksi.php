16df7c1face5ae44032ec0bddc7dd9df8506af37
qorib-faris <smkamt2@gmail.com>
2026-04-16 08:03:56 -0700

    pembelajaran-aplikasi

--- koneksi.php
+++ koneksi.php
@@ -7,4 +7,4 @@ $database = 'manajemen-perpustakaan';
 $conn = new mysqli($host, $username, $password, $database);
 if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
-}
+} 
